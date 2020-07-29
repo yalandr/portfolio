e0f23fc98cf06069eccefdcd60012e83d0f25482
yalandr <52127093+yalandr@users.noreply.github.com>
2020-07-29 22:22:57 +0300

    Add files via upload
    files of portfolio

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*==== Parallax ==============*/
+/*function parallax(event) {
+	this.querySelectorAll('.layer').forEach(layer => {
+		let speed = layer.getAttribute('data-speed');
+		layer.style.transform = `translateY(${event.clientY*speed/10}px)`
+	});
+}
+
+document.addEventListener('scroll', parallax);*/
