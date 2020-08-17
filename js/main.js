47813f379d58509da2386dab6a83fc8556913a77
yalandr <52127093+yalandr@users.noreply.github.com>
2020-08-17 21:36:23 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,23 +2,25 @@
 
 /*==== Mobile Menu ============================*/
 
-let menuBtn = document.querySelector('.mobile-menu-btn'),
-	mobileMenu = document.querySelector('.mobile-menu'),
-	menuClose = document.querySelector('.close-icon'),
-	mobileMenuItem = document.querySelectorAll('.mobile-nav-item');
-	
+const menuBtn = document.querySelector('.mobile-menu-btn'),
+ mobileMenu = document.querySelector('.mobile-menu'),
+ menuClose = document.querySelector('.close-icon'),
+ menuList = document.querySelector('.mobile-nav-list');
 
 menuBtn.onclick = function() {
-	 mobileMenu.style.left = '0';
+  mobileMenu.style.left = '0';
+  menuClose.style.transform = 'rotate(90deg)';
 }
 
 menuClose.onclick = function() {
-	 mobileMenu.style.left = '-100%';
+  mobileMenu.style.left = '-100%';
+  menuClose.style.transform = 'rotate(-90deg)';
 }
 
-mobileMenuItem.onclick = function() {
-	mobileMenu.style.left = '-100%';
+menuList.onclick = function(e) {
+ if (e.target.classList.contains('mobile-menu-link')) {
+  mobileMenu.style.left = '-100%';
+  menuClose.style.transform = 'rotate(-90deg)';
+ }
 }
-
-/*==== Skills Option ==========================*/
 
